0f1296221d4ddb1c4af374cefdea575dd7ce38f4
Kharisma Andini Putri <100285562+kharizmaaandin@users.noreply.github.com>
2023-05-21 21:31:54 +0700

    Praktikum 8

--- Pratikum 8/generic/BangunDatarGeneric.java
+++ Pratikum 8/generic/BangunDatarGeneric.java
@@ -6,13 +6,13 @@
  * Deskripsi 	: Kelas konstruksi generic untuk BangunDatar
 */
 
-public class BangunDatarGeneric <T extends BangunDatar>{
-	private T bangunDatar;
+public class BangunDatarGeneric <T1234 extends BangunDatar>{
+	private T1234 bangunDatar;
 
-	public void set (T tipeBangunDatar){
+	public void set (T1234 tipeBangunDatar){
 		bangunDatar = tipeBangunDatar;
 	}
-	public T get(){
+	public T1234 get(){
 		return bangunDatar;
 	}
 	public double hitungKeliling(){
@@ -20,7 +20,7 @@ public class BangunDatarGeneric <T extends BangunDatar>{
 	}
 }
 
-// TUGAS
+// Tugas
 // Cobalah pada saat praktikum : Gantilah ‘T’ (dan segala T) pada BangunDatarGeneric.java diganti 
 // dengan karakter yang lain seperti, T1,T2,T1234. Simpulkan apa yang anda lakukan !
 
